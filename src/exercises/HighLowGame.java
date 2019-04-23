2bbb4bfb3d4a50cd01ce109a83c179ea2d3c71e5
jelenam86 <jelena.mat86@gmail.com>
2019-04-23 17:42:08 +0200

    Added code for simple number HighLow game

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,6 +1,22 @@
 package exercises;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
+	public static void main(String[] args) {
 
+		int rand = new Random().nextInt();
+
+		String answer = JOptionPane.showInputDialog("Guess is next number higher or lower than " + rand);
+		int number = new Random().nextInt();
+
+		if ((answer.equals("higher") && number > rand) || (answer.equals("lower") && number < rand)) {
+			JOptionPane.showMessageDialog(null, "Correct! Next number is " + number);
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong! Next number is " + number);
+		}
+	}
 }
