da02fec1fee7423e9dc0da86989bd853f145cd03
jelenam86 <jelena.mat86@gmail.com>
2019-04-23 17:43:43 +0200

    Added code for five words to spell and score to count

--- src/exercises/SpeakAndSpell.java
+++ src/exercises/SpeakAndSpell.java
@@ -1,12 +1,14 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 import voce.SpeechSynthesizer;
 
 /**
  * The first Speak & Spell was introduced at the summer Consumer Electronics
  * Show in June 1978, making it one of the earliest hand-held electronic devices
- * with a visual display to use interchangeable game cartridges. 
- * It was my favorite game as a kid.
+ * with a visual display to use interchangeable game cartridges. It was my
+ * favorite game as a kid.
  * 
  * https://www.youtube.com/watch?v=qM8FcN0aAvU
  **/
@@ -14,19 +16,55 @@ import voce.SpeechSynthesizer;
 public class SpeakAndSpell {
 
 	public static void main(String[] args) {
-		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
-		// 2. Catch the user's answer in a String
-
-		// 3. If the user spelled the word correctly, speak "correct"
-
-		// 4. Otherwise say "wrong"
-
-		// 5. Repeat the process for other words
 		
-		// 6. Calculate the user's score and give it to them at the end of the game 
+		int score = 0;
 		
-		
+		speak("Spell mandlebrot");
+		String answer1 = JOptionPane.showInputDialog("Type your answer:");
+		if (answer1.equals("mandlebrot")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+
+		speak("Spell handkerchief");
+		String answer2 = JOptionPane.showInputDialog("Type your answer:");
+		if (answer2.equals("handkerchief")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+
+		speak("Spell rhythm");
+		String answer3 = JOptionPane.showInputDialog("Type your answer:");
+		if (answer3.equals("rhythm")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+
+		speak("Spell occurred");
+		String answer4 = JOptionPane.showInputDialog("Type your answer:");
+		if (answer4.equals("occurred")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+
+		speak("Spell millennium");
+		String answer5 = JOptionPane.showInputDialog("Type your answer:");
+		if (answer5.equals("millennium")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+		}
+
+		JOptionPane.showMessageDialog(null, "Your score: " + score);
 	}
 
 	static void speak(String words) {
