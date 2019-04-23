6a47aba1d9232a519f5652a7804a6cee19aebfcb
jelenam86 <jelena.mat86@gmail.com>
2019-04-23 18:49:49 +0200

    Code corrected to meet platform requirements

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -8,15 +8,26 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 
-		int rand = new Random().nextInt();
+		int rand = new Random().nextInt(100) + 1;
+		System.out.println(rand);
+		int number = 0;
 
-		String answer = JOptionPane.showInputDialog("Guess is next number higher or lower than " + rand);
-		int number = new Random().nextInt();
+		for (int i = 0; i < 5; i++) {
+			String answer = JOptionPane.showInputDialog("Guess number between 1 and 100");
+			number = Integer.parseInt(answer);
 
-		if ((answer.equals("higher") && number > rand) || (answer.equals("lower") && number < rand)) {
-			JOptionPane.showMessageDialog(null, "Correct! Next number is " + number);
-		} else {
-			JOptionPane.showMessageDialog(null, "Wrong! Next number is " + number);
+			if (number > rand && i != 4)
+				JOptionPane.showMessageDialog(null, number + " is too high. " + (5 - i - 1) + " more tries.");
+			else if (number < rand && i != 4)
+				JOptionPane.showMessageDialog(null, number + " is too low. " + (5 - i - 1) + " more tries.");
+			else
+				break;
 		}
+
+		if (number == rand)
+			JOptionPane.showMessageDialog(null, "You won!");
+		else
+			JOptionPane.showMessageDialog(null, "You lose!");
+
 	}
 }
