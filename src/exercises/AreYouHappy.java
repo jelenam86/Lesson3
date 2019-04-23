404e85f8b956dddad255a3c999a510672727d42f
jelenam86 <jelena.mat86@gmail.com>
2019-04-23 17:39:19 +0200

    Added code based on the diagram in the doc directory

--- src/exercises/AreYouHappy.java
+++ src/exercises/AreYouHappy.java
@@ -1,7 +1,22 @@
 package exercises;
 
+import javax.swing.JOptionPane;
+
 /* See the diagram in the "doc" directory. */
 
 public class AreYouHappy {
+	public static void main(String[] args) {
 
+		String answer = JOptionPane.showInputDialog("Are you happy?");
+		if (answer.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you doing");
+		} else {
+			String answer2 = JOptionPane.showInputDialog("Do you want to be happy?");
+			if (answer2.equals("yes")) {
+				JOptionPane.showMessageDialog(null, "Change something");
+			} else {
+				JOptionPane.showMessageDialog(null, "Keep doing whatever you doing");
+			}
+		}
+	}
 }
