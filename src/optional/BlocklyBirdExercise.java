aab06f370b6f2c0b60d3ff18294c220b3d07028b
jelenam86 <jelena.mat86@gmail.com>
2019-04-24 19:28:11 +0200

    Added steps for solving puzzle

--- src/optional/BlocklyBirdExercise.java
+++ src/optional/BlocklyBirdExercise.java
@@ -4,4 +4,37 @@ package optional;
 *			bit.ly/bird-puzzle
 */
 
-public class BlocklyBirdExercise {}
+/*
+ * 1. heading 60
+ * 2. if(does not have worm) heading 0;
+ * 	  else heading 90;
+ * 3. if(does not have worm) heading 300;
+ * 	  else heading 60;
+ * 4. if(x < 80) heading 0;
+ * 	  else heading 270;
+ * 5. if(y > 20) heading 270;
+ * 	  else heading 180;
+ * 6. if(does not have worm) heading 330;
+ * 	  else if(y < 80) heading 90;
+ * 	  else heading 180;
+ * 7. if(y > 60) heading 210;
+ * 	  else if(y > 20) heading 270;
+ * 	  else if(does not have worm) heading 0;
+ * 	  else heading 180;
+ * 8. if (does not have worm && x < 50) heading 45;
+ * 	  else if (does not have worm) heading 315;
+ * 	  else if (y < 55) heading 135;
+ * 	  else heading 45;
+ * 9. if (does not have worm && x > 60) heading 190;
+ * 	  else if (does not have worm) heading 270;
+ * 	  else if (x < 35) heading 75;
+ * 	  else heading 300;
+ * 10. if (does not have worm && x < 40) heading 75;
+ * 	  else if (does not have worm && x < 80) heading 315;
+ * 	  else if (y < 98 && x > 30) heading 137;
+ * 	  else heading 270;
+ */
+
+public class BlocklyBirdExercise {
+	
+}
