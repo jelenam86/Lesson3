268eea9ace61b2fe3d8101d0633d3008a0470fac
jelenam86 <jelena.mat86@gmail.com>
2019-04-24 19:33:45 +0200

    Addded code and compliments

--- src/optional/Validation.java
+++ src/optional/Validation.java
@@ -2,20 +2,28 @@ package optional;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Validation {
+
 	public static void main(String[] args) {
 
 		// 1. Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		int randomNumber = new Random().nextInt(5);
-		// 2. On paper, write all the numbers that get printed when you run this class
-		System.out.println(randomNumber);
 
-		// 3. Use the randomNumber to give the user a random compliment.
+		for (int i = 0; i < 10; i++) {
+			int randomNumber = new Random().nextInt(5);
+			System.out.println(randomNumber);
 
-		// 4. Repeat all the code above 10 times
-		
-		// 5. Find someone to test out your program. They will like it :)
+			if (randomNumber == 0)
+				JOptionPane.showMessageDialog(null, "You! You are great!");
+			else if (randomNumber == 1)
+				JOptionPane.showMessageDialog(null, "You're a gift to those around you.");
+			else if (randomNumber == 2)
+				JOptionPane.showMessageDialog(null, "You should be proud of yourself.");
+			else if (randomNumber == 3)
+				JOptionPane.showMessageDialog(null, "You should be thanked more often. So thank you!!");
+			else
+				JOptionPane.showMessageDialog(null, "There's ordinary, and then there's you.");
+		}
 	}
 }
-
-
