7f71dbd1c967ecebfcfc791f1bb213fa6986048d
jelenam86 <jelena.mat86@gmail.com>
2019-04-24 19:32:48 +0200

    Added code, riddles and answers

--- src/optional/TheRiddler.java
+++ src/optional/TheRiddler.java
@@ -1,21 +1,60 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class TheRiddler {
+
+	// Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
+		int score = 0;
+		String[] riddles = new String[10];
+		String[] answers = new String[10];
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		riddles[0] = "What can travel around the world while staying in a corner?";
+		answers[0] = "a stamp";
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		riddles[1] = "What has hands but can not clap?";
+		answers[1] = "a clock";
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+		riddles[2] = "You can drop me from the tallest building and I'll be fine, \nbut if you drop me in water I die. What am I?";
+		answers[2] = "paper";
 
-		// 6. Add some more riddles
+		riddles[3] = "Forward I am heavy, but backward I am not. What am I?";
+		answers[3] = "forward I am ton, backwards I am not";
 
-		// 2. Make a pop up to show the score.
-		
+		riddles[4] = "Imagine you are in a dark room. How do you get out?";
+		answers[4] = "stop imagining.";
+
+		riddles[5] = "What occurs once in every minute, twice in every moment, yet never in a thousand years?";
+		answers[5] = "the letter m";
+
+		riddles[6] = "Alive without breath,\n" + "As cold as death;\n" + "Never thirsty, ever drinking,\n"
+				+ "All in mail never clinking.\n\n" + "\"The Hobbit\" by J. R. R. Tolkien";
+		answers[6] = "fish";
+
+		riddles[7] = "What is always coming but never arrives?";
+		answers[7] = "tomorrow";
+
+		riddles[8] = "What invention lets you look right through a wall?";
+		answers[8] = "a window";
+
+		riddles[9] = "Paul's height is six feet, he's an assistant at a butcher's shop, \nand wears size 9 shoes. What does he weigh?";
+		answers[9] = "meat";
+
+		for (int i = 0; i < riddles.length; i++) {
+			String answer = JOptionPane.showInputDialog(riddles[i]);
+			if (answer.equals(answers[i])) {
+				JOptionPane.showMessageDialog(null, "Correct!");
+				score++;
+			} else {
+				JOptionPane.showMessageDialog(null, "Wrong!");
+				JOptionPane.showMessageDialog(null, "Correct answer is: " + answers[i]);
+			}
+		}
+
+		JOptionPane.showMessageDialog(null, "Your score: " + score + "/" + riddles.length);
+
 	}
 }
-
